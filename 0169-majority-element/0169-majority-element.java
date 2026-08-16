c7e47215fd50c8bc3a83928887fbe1538f3795f6
Naresh Sathe <nareshsathe91667@gmail.com>
2026-08-16 13:19:38 +0530

    Time: 1884 ms (7.15%), Space: 55.8 MB (32.18%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+    int n = nums.length;
+
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+
+        // count how many times nums[i] appears
+        for (int j = 0; j < n; j++) {
+            if (nums[j] == nums[i]) {
+                count++;
+            }
+        }
+
+          if (count > n / 2) {
+            return nums[i];
+        }
+    
+    }
+
+    return -1;
+    }
+}
+
+    
